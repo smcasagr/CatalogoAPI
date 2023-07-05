3f39940532acdaaeb432ba40223de45cb564bcab
smcasag <samuel.mcasagrande@gmail.com>
2023-07-05 17:27:31 -0300

    ProdutoController template created

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,20 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context; // injetando a instância de db context no controlador
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
