2c841f6718923af177e108ed26ee5880f98f3ade
smcasag <samuel.mcasagrande@gmail.com>
2023-07-06 10:22:54 -0300

    Delete product (DELETE) implemented

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -68,5 +68,20 @@ namespace APICatalogo.Controllers
 
             return Ok(produto);
         }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        { 
+            var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
+            if (produto is null )
+            {
+                return NotFound($"Produto não localizado! - ID: {id}");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
     }
 }
